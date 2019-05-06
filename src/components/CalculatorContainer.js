2dfc9047384c81b8609bbf34c215e7266476afe2
shohei <shoheimoment@gmail.com>
2019-05-07 06:38:48 +0900

    ボタンの配置を変更

--- src/components/CalculatorContainer.js
+++ src/components/CalculatorContainer.js
@@ -10,27 +10,37 @@ class CalculatorContainer extends Component {
     const { calculator, actions } = this.props;
     return (
       <div>
-        <div>
-          <NumBtn n={1} onClick={() => actions.onNumClick(1)} />
-          <NumBtn n={2} onClick={() => actions.onNumClick(2)} />
-          <NumBtn n={3} onClick={() => actions.onNumClick(3)} />
-        </div>
-        <div>
-          <NumBtn n={4} onClick={() => actions.onNumClick(4)} />
-          <NumBtn n={5} onClick={() => actions.onNumClick(5)} />
-          <NumBtn n={6} onClick={() => actions.onNumClick(6)} />
-        </div>
-        <div>
-          <NumBtn n={7} onClick={() => actions.onNumClick(7)} />
-          <NumBtn n={8} onClick={() => actions.onNumClick(8)} />
-          <NumBtn n={9} onClick={() => actions.onNumClick(9)} />
-        </div>
-        <div>
-          <NumBtn n={0} onClick={() => actions.onNumClick(0)} />
-          <PlusBtn onClick={actions.onPlusClick} />
-          <MinusBtn onClick={actions.onMinusClick} />
-          <ClearBtn onClick={actions.onClearClick} />
-        </div>
+        <table>
+          <tr>
+            <td colspan="3"><ClearBtn onClick={actions.onClearClick} /></td>
+            <td><button>÷</button></td>
+          </tr>
+          <tr>
+            <td><NumBtn n={7} onClick={() => actions.onNumClick(7)} /></td>
+            <td><NumBtn n={8} onClick={() => actions.onNumClick(8)} /></td>
+            <td><NumBtn n={9} onClick={() => actions.onNumClick(9)} /></td>
+            <td><button>×</button></td>
+          </tr>
+          <tr>
+            <td><NumBtn n={4} onClick={() => actions.onNumClick(4)} /></td>
+            <td><NumBtn n={5} onClick={() => actions.onNumClick(5)} /></td>
+            <td><NumBtn n={6} onClick={() => actions.onNumClick(6)} /></td>
+            <td><MinusBtn onClick={actions.onMinusClick} /></td>
+          </tr>
+          <tr>
+            <td><NumBtn n={1} onClick={() => actions.onNumClick(1)} /></td>
+            <td><NumBtn n={2} onClick={() => actions.onNumClick(2)} /></td>
+            <td><NumBtn n={3} onClick={() => actions.onNumClick(3)} /></td>
+            <td><PlusBtn onClick={actions.onPlusClick} /></td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <NumBtn n={0} onClick={() => actions.onNumClick(0)} />
+            </td>
+            <td><button>.</button></td>
+            <td><button>=</button></td>
+          </tr>
+        </table>
         <Result result={calculator.showingResult ? calculator.resultValue : calculator.inputValue} />
       </div>
     );
